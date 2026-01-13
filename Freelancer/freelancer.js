e354baf4e39d6e4b7aae9ba38bb59d1a30525955
Deepikav123 <deepika.v.tech@gmail.com>
2026-01-13 17:07:40 +0530

    Made submit button interactive

--- Freelancer/freelancer.js
+++ Freelancer/freelancer.js
@@ -4,19 +4,54 @@ let code = ``;
 function display() {
     gigs.forEach((element) => {
 
-        code += ` 
-         <div class="displayGig">
-                    <div class="title">${element.title}</div>
-                    <div class="budget">Budget:₹${element.budget}</div>
-                    <div class="description">${element.description}</div>
-                    <div class="bidButton">
-                        <button class="bid">
-                            Bid
-                        </button>
-                    </div>
-                </div>`
+        code += `  <div class="displayGig">
+        <div class="title">${element.title}</div>
+        <div class="budget">Budget:₹${element.budget}</div>
+        <div class="description">${element.description}</div>
+        <div class="bidButton buttonId-${element.id}">
+            <button class="bid id-${element.id}" data-bid="${element.id}">
+                Bid
+            </button>
+
+            <div class="bidForm">
+            <div class="form">
+                <div class="amount">
+                    Bid amount:
+                    <input type="number" class="inputBox">
+                </div>
+
+                <div class=" messageBox">
+                    Message:
+                    <textarea class="message" rows="5" cols="50"></textarea>
+                </div>
+                <div class="submitButton" >
+                    <button class="submit" data-sid=${element.id}>Submit</button>
+                </div>
+
+            </div>
+        </div>
+        </div>
+        
+
+    </div>`
+
     })
     document.querySelector('.displayGigs').innerHTML = code;
+
+    document.querySelectorAll('.bid').forEach((ele) => {
+        let id = ele.dataset.bid;
+        ele.addEventListener('click', () => {
+            document.querySelector(`.buttonId-${id}`).classList.add('b');
+        })
+    })
+
+    document.querySelectorAll('.submit').forEach((element) => {
+        let id = element.dataset.sid;
+        element.addEventListener('click', () => {
+            document.querySelector(`.buttonId-${id}`).classList.remove('b');
+
+        })
+    })
 
 }
 display()
